5e8c0f561983fb1551a6f3cedd78fcd231f4a0c7
lichin-lin <vic20087cjimlin@gmail.com>
2018-07-12 02:06:22 +0800

    add: data

--- src/layouts/taiwan-golden-melody-awards-29/data.js
+++ src/layouts/taiwan-golden-melody-awards-29/data.js
@@ -34,127 +34,187 @@ export const data = [
         src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
       }
     ]
+  },
+  {
+    name: "best-female-taiwanese-singer",
+    bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-female-taiwanese-singer.jpg`),
+    nominationList: [
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
+      }
+    ],
+    designerList: [
+      {
+        title: `插畫設計：王春子 WangChuenTz`,
+        brief: `自由插畫家，為了作品的一氣呵成，也接平面設計。兒子出生後開始創作繪本。也和朋友持續製作發行獨立刊物《風土痣》。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/1.jpg`)
+      },
+      {
+        title: `動態設計：許峻良 Jim Hsu`,
+        brief: `Jim Hsu，吉姆許，動態圖像設計師。大學畢業後赴美探險，從財金人轉投胎成設計人，偏愛結合2D與3D的動態圖像設計，現正燃燒熱情自由接案中。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
+      }
+    ]
+  },
+  {
+    name: "song-of-the-year",
+    bg: require(`assets/Taiwan-Golden-Melody-Awards-29/song-of-the-year.jpg`),
+    nominationList: [
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
+      }
+    ],
+    designerList: [
+      {
+        title: `插畫設計：王春子 WangChuenTz`,
+        brief: `自由插畫家，為了作品的一氣呵成，也接平面設計。兒子出生後開始創作繪本。也和朋友持續製作發行獨立刊物《風土痣》。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/1.jpg`)
+      },
+      {
+        title: `動態設計：許峻良 Jim Hsu`,
+        brief: `Jim Hsu，吉姆許，動態圖像設計師。大學畢業後赴美探險，從財金人轉投胎成設計人，偏愛結合2D與3D的動態圖像設計，現正燃燒熱情自由接案中。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
+      }
+    ]
+  },
+  {
+    name: "best-lyrics",
+    bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-lyrics.jpg`),
+    nominationList: [
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
+      }
+    ],
+    designerList: [
+      {
+        title: `插畫設計：王春子 WangChuenTz`,
+        brief: `自由插畫家，為了作品的一氣呵成，也接平面設計。兒子出生後開始創作繪本。也和朋友持續製作發行獨立刊物《風土痣》。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/1.jpg`)
+      },
+      {
+        title: `動態設計：許峻良 Jim Hsu`,
+        brief: `Jim Hsu，吉姆許，動態圖像設計師。大學畢業後赴美探險，從財金人轉投胎成設計人，偏愛結合2D與3D的動態圖像設計，現正燃燒熱情自由接案中。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
+      }
+    ]
+  },
+  {
+    name: "best-mandarin-album",
+    bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-mandarin-album.jpg`),
+    nominationList: [
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
+      }
+    ],
+    designerList: [
+      {
+        title: `插畫設計：王春子 WangChuenTz`,
+        brief: `自由插畫家，為了作品的一氣呵成，也接平面設計。兒子出生後開始創作繪本。也和朋友持續製作發行獨立刊物《風土痣》。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/1.jpg`)
+      },
+      {
+        title: `動態設計：許峻良 Jim Hsu`,
+        brief: `Jim Hsu，吉姆許，動態圖像設計師。大學畢業後赴美探險，從財金人轉投胎成設計人，偏愛結合2D與3D的動態圖像設計，現正燃燒熱情自由接案中。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
+      }
+    ]
+  },
+  {
+    name: "best-taiwanese-album",
+    bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-taiwanese-album.jpg`),
+    nominationList: [
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
+      },
+      {
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
+      }
+    ],
+    designerList: [
+      {
+        title: `插畫設計：王春子 WangChuenTz`,
+        brief: `自由插畫家，為了作品的一氣呵成，也接平面設計。兒子出生後開始創作繪本。也和朋友持續製作發行獨立刊物《風土痣》。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/1.jpg`)
+      },
+      {
+        title: `動態設計：許峻良 Jim Hsu`,
+        brief: `Jim Hsu，吉姆許，動態圖像設計師。大學畢業後赴美探險，從財金人轉投胎成設計人，偏愛結合2D與3D的動態圖像設計，現正燃燒熱情自由接案中。`,
+        src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement-designer/2.jpg`)
+      }
+    ]
   }
-  // {
-  //   name: "best-female-taiwanese-singer",
-  //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-female-taiwanese-singer.jpg`),
-  //   nominationList: [
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "song-of-the-year",
-  //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/song-of-the-year.jpg`),
-  //   nominationList: [
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "best-lyrics",
-  //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-lyrics.jpg`),
-  //   nominationList: [
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "best-mandarin-album",
-  //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-mandarin-album.jpg`),
-  //   nominationList: [
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "best-taiwanese-album",
-  //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-taiwanese-album.jpg`),
-  //   nominationList: [
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/1.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/2.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/3.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/4.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/5.jpg`)
-  //     },
-  //     {
-  //       src: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement/6.jpg`)
-  //     }
-  //   ]
-  // },
   // {
   //   name: "best-arrangement",
   //   bg: require(`assets/Taiwan-Golden-Melody-Awards-29/best-arrangement.jpg`),
